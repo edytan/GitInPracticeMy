249a25c88aca86bf73cdc441e78c0af211da7ba3
Edmond Tanase <Edmond.Tanase@kronos.com>
2014-07-12 09:14:05 -0400

    push test from a different clone

--- groovy/hotspots/extractDefects/extractDefects.groovy
+++ groovy/hotspots/extractDefects/extractDefects.groovy
@@ -1,6 +1,6 @@
 //hello.groovy
 println "hello, world"
-arguments = ['mama', 'mia', 'tata', 'tia','gia']
+arguments = ['mama', 'mia', 'tata', 'tia','gia', 'pia']
 
 for (argument in arguments ) {
   println "Argument:" + argument;
