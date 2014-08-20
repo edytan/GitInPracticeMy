9841897e3c6e9b63a92e116f2a1c1741208476f4
Edmond Tanase <Edmond.Tanase@kronos.com>
2014-08-20 14:42:52 -0400

    20/08/14

--- groovy/hotspots/extractDefects/extractDefects.groovy
+++ groovy/hotspots/extractDefects/extractDefects.groovy
@@ -1,7 +1,7 @@
 //hello.groovy
 println "hello, world"
 
-arguments = ['mama', 'mia', 'tata', 'tia','gia', 'pia', 'rex', 'cara']
+arguments = ['mama', 'mia', 'tata', 'tia','gia', 'pia', 'rex', 'cara','mia', 'pia']
 
 for (argument in arguments ) {
   println "Argument:" + argument;
